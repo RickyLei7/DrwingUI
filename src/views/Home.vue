2bd62f7af0dd6d6981380a6a4d06b32990ac9328
RickyLei7 <Rickylei7@gmail.com>
2020-12-16 14:23:36 -0700

    topNav Css Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topNav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">Logo</div>
+      <ul class="menu">
+        <li>Menu#1</li>
+        <li>Menu#2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>Drwing UI</h1>
@@ -18,5 +21,19 @@
 <style lang="scss" scoped>
 .topNav{
   border: 1px solid red;
+  background: pink;
+  padding: 16px;
+  > .logo{
+    max-width: 6em;
+    margin-right: auto;
+  }
+  .menu{
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li{
+      margin: 0 1em;
+    }
+  }
 }
 </style>
