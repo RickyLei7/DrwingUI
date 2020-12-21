ad2b011671de58c34222fe46f919255cca01e480
RickyLei7 <Rickylei7@gmail.com>
2020-12-21 16:01:16 -0700

    create Button.vue

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-   BUtton Dwmo
+   Button Demo
+  </div>
+  <h1>Demo#1</h1>
+  <div>
+    <Button>Hello</Button>
   </div>
 </template>
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components:{Button}
+}
+</script>
