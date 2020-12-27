4d68bf3e1c2a3900bd79705fb5ebefefd114a938
RickyLei7 <Rickylei7@gmail.com>
2020-12-26 18:52:56 -0700

    Button component done

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,9 +5,30 @@
   <h1>Demo#1</h1>
   <div>
     <Button>Hello</Button>
-    <Button drwing-theme="button">Hello</Button>
-    <Button drwing-theme="link">Hello</Button>
-    <Button drwing-theme="text">Hello</Button>
+    <Button theme="button">Hello</Button>
+    <Button theme="link">Hello</Button>
+    <Button theme="text">Hello</Button>
+  </div>
+  <h1>Demo#2</h1>
+  <div>
+    <Button size="big">BigBig</Button>
+    <Button>Normal</Button>
+    <Button size="small">SmallSmall</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">BigBig</Button>
+    <Button theme="link">Normal</Button>
+    <Button theme="link" size="small">SmallSmall</Button>
+  </div>
+  <div>
+    <Button theme="text" size="big">BigBig</Button>
+    <Button theme="text">Normal</Button>
+    <Button theme="text" size="small">SmallSmall</Button>
+  </div>
+  <h1>Demo#3</h1>
+  <div>
+    <Button loading>Loading</Button>
+    <Button>Loaded</Button>
   </div>
 </template>
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,36 @@
 <template>
-  <button class="drwing-button" :class="{[`drwing-theme-${theme}`]: theme}">
+  <button class="drwing-button" :class="classes">
+    <span v-if="loading" class="drwing-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
+  },
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`drwing-theme-${theme}`]: theme,
+        [`drwing-size-${size}`]: size,
+      };
+    });
+    return {classes};
   }
 };
 </script>
@@ -51,5 +72,54 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.drwing-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%)
+    }
+  }
+
+  &.drwing-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.drwing-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.drwing-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+}
+.drwing-loadingIndicator{
+  width: 14px;
+  height: 14px;
+  display: inline-block;
+  margin-right: 4px;
+  border-radius: 8px;
+  border-color: $blue $blue $blue transparent;
+  border-style: solid;
+  border-width: 2px;
+  animation: drwing-spin 2s infinite linear;
+}
+@keyframes drwing-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
