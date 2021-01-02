01993627711180f5488f179fb03e7bb0285384d6
RickyLei7 <Rickylei7@gmail.com>
2021-01-01 20:30:49 -0700

    Create Dialog component

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-Dialog Demo
+    <h1>Demo#1</h1>
+    <Dialog></Dialog>
   </div>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+
+export default {
+  components: {Dialog}
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+      <header>Title</header>
+      <main>
+        <p>First line text</p>
+        <p>Second line text</p>
+      </main>
+      <footer>
+        <Button>Ok</Button>
+        <Button>Cancel</Button>
+      </footer>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  components: {Button}
+};
+</script>
